78257ad2ef289add08790d5602e3b20f5d69ddb6
Yepavlov <ff2-15@ukr.net>
2024-02-11 15:25:57 -0800

    BDD_and_DDT_testing with allure report

--- rest/lambdatest.py
+++ rest/lambdatest.py
@@ -1,50 +1,96 @@
+import json
+from logging import getLogger
 
 import requests
+
+import allure
 
 
 class LambdaTestService:
     BASE_URL = "https://test-backend.lambdatest.com/api/dev-tools/"
 
+    def __init__(self):
+        self._log = getLogger(__name__)
+
+    @allure.step("Send a POST request to {path}")
     def _send_request(self, path, input_key, input_str):
+        """
+        Send Post request with data
+        :param path: endpoint for url
+        :param input_key: input_key for request
+        :param input_str: string for key
+        :return: response
+        """
         url = self.BASE_URL + path
         response = requests.post(url, data={input_key: input_str})
+        allure.attach(url, "Full url", allure.attachment_type.TEXT)
+        self._log.info(f"Send request to url: {url} with data: {input_key}: {input_str}")
+
         return response
 
+    @allure.step("Send a POST request to convert JSON to XML")
     def convert_json_to_xml(self, input_str: str) -> str:
+        self._log.info(f"Send a Post request to endpoint: 'json-to-xml' with data: 'input-str': {input_str}")
         response = self._send_request("json-to-xml", "input-str", input_str).text
+        allure.attach(input_str, "Input JSON", allure.attachment_type.JSON)
+        allure.attach(response, "Output XML", allure.attachment_type.XML)
         return response
 
+    @allure.step("Send a POST request to minify XML")
     def minify_xml(self, input_str: str) -> str:
+        self._log.info(f"Send a POST request to endpoint: 'minify-xml' with data: 'input-str': {input_str}")
         response = self._send_request("minify-xml", "input-str", input_str).json()[
             "minify_data"
         ]
+        allure.attach(input_str, "Input XML", allure.attachment_type.XML)
+        allure.attach(response, "Output minified XML", allure.attachment_type.XML)
         return response
 
+    @allure.step("Send a POST request to extract Text from JSON")
     def extract_text_from_json(self, input_str: str) -> str:
+        self._log.info(f"Send a POST request to endpoint: 'extract-text-json' with data: 'input-str': {input_str}")
         response = self._send_request(
             "extract-text-json", "input-str", input_str
         ).json()["data"]
+        allure.attach(input_str, "Input JSON", allure.attachment_type.JSON)
+        allure.attach(response, "Extracted TEXT", allure.attachment_type.TEXT)
         return response
 
+    @allure.step("Send a POST request to validate YAML")
     def yaml_validator(self, input_str: str) -> str:
+        self._log.info(f"Send a POST request to endpoint: 'yaml-validator' with data: 'yaml-str': {input_str}")
         response = self._send_request("yaml-validator", "yaml-str", input_str).json()[
             "message"
         ]
+        allure.attach(input_str, "Input YAML", allure.attachment_type.YAML)
+        allure.attach(response, "Message about checking the entered YAML", allure.attachment_type.TEXT)
         return response
 
+    @allure.step("Send a POST request to convert JSON to YAML")
     def convert_json_to_yaml(self, input_str: str) -> str:
+        self._log.info(f"Send a POST request to endpoint: 'json-to-yaml' with data: 'json-str': {input_str}")
         response = self._send_request("json-to-yaml", "json-str", input_str).json()[
             "data"
         ]
+        allure.attach(input_str, "Input JSON", allure.attachment_type.JSON)
+        allure.attach(response, "Output YAML", allure.attachment_type.YAML)
         return response
 
+    @allure.step("Send a POST request to convert XML to YAML")
     def convert_xml_to_yaml(self, input_str: str) -> str:
+        self._log.info(f"Send a POST request to endpoint: 'xml-to-yaml' with data: 'xml-str': {input_str}")
         response = self._send_request("xml-to-yaml", "xml-str", input_str).json()[
             "data"
         ]
+        allure.attach(input_str, "Input XML", allure.attachment_type.XML)
+        allure.attach(response, "Output YAML", allure.attachment_type.YAML)
         return response
 
+    @allure.step("Send a POST request to convert YAML to XML")
     def convert_yaml_to_json(self, input_str: str) -> str:
+        self._log.info(f"Send a POST request to endpoint: 'yaml-to-json' with data: 'yaml-str': {input_str}")
         response = self._send_request("yaml-to-json", "yaml-str", input_str).json()
         response_data = response.get("data")
+        allure.attach(input_str, "Input YAML", allure.attachment_type.YAML)
+        allure.attach(json.dumps(response_data), "Output JSON", allure.attachment_type.TEXT)
         return response_data
--- tests/test_lambdatest.py
+++ tests/test_lambdatest.py
@@ -1,9 +1,11 @@
 import json
 import os
-
+from logging import getLogger
 from pytest_bdd import parsers, scenarios, given, when, then
-
+import allure
 from utils.file_utils import read_data_file, get_root_path
+logger = getLogger(__name__)
+
 
 feature_path = os.path.join(get_root_path(), "tests", "features", "lambdatest.feature")
 
@@ -12,67 +14,100 @@ scenarios(feature_path)
 
 @given(parsers.parse("prepare a JSON file: {json_file_name}"), target_fixture="context")
 def a_json_file_is_prepared(json_file_name, context):
-    input_json = read_data_file(f"json/{json_file_name}")
-    context["input_json"] = input_json
+    with allure.step("Prepare a JSON file: {json_file_name}"):
+        logger.info(f"prepare a JSON file: {json_file_name}")
+        input_json = read_data_file(f"json/{json_file_name}")
+        context["input_json"] = input_json
+        allure.attach(json_file_name, "JSON file name", allure.attachment_type.TEXT)
+        allure.attach(json.dumps(input_json, indent=2), name="Input JSON", attachment_type=allure.attachment_type.TEXT)
 
-    return context
+        return context
 
 
 @given(parsers.parse("prepare a XML file: {xml_file_name}"), target_fixture="context")
 def a_xml_file_is_prepared(xml_file_name, context):
-    expected_xml = read_data_file(f"xml/{xml_file_name}")
-    context["expected_xml"] = expected_xml
+    with allure.step("Prepare a XML file: {xml_file_name}"):
+        logger.info(f"Prepare a XML file: {xml_file_name}")
+        expected_xml = read_data_file(f"xml/{xml_file_name}")
+        context["expected_xml"] = expected_xml
+        allure.attach(xml_file_name, "XML file name", allure.attachment_type.XML)
+        allure.attach(json.dumps(expected_xml, indent=2), "Input XML", allure.attachment_type.TEXT)
 
-    return context
+        return context
 
 
 @given(parsers.parse("prepare a TXT file: {txt_file_name}"), target_fixture="context")
 def a_txt_file_is_prepared(txt_file_name, context):
-    expected_txt = read_data_file(f"txt/{txt_file_name}")
-    context["expected_txt"] = expected_txt
+    with allure.step("Prepare a TXT file: {txt_file_name}"):
+        logger.info(f"prepare a TXT file: {txt_file_name}")
+        expected_txt = read_data_file(f"txt/{txt_file_name}")
+        context["expected_txt"] = expected_txt
+        allure.attach(txt_file_name, "TXT file name", allure.attachment_type.TEXT)
+        allure.attach(json.dumps(expected_txt, indent=2), "Input TEXT", allure.attachment_type.TEXT)
 
-    return context
+        return context
 
 
 @when("converting JSON to XML", target_fixture="context")
 def convert_json_to_xml(context, lambda_test_service):
-    actual_xml = lambda_test_service.convert_json_to_xml(context["input_json"])
-    context["actual_xml"] = actual_xml
+    with allure.step("Convert JSON to XML"):
+        actual_xml = lambda_test_service.convert_json_to_xml(context["input_json"])
+        context["actual_xml"] = actual_xml
+        logger.info(f"Convert JSON: {context['input_json']} to XML: {context['actual_xml']}")
+        allure.attach(context["input_json"], "Input JSON", allure.attachment_type.JSON)
+        allure.attach(context['actual_xml'], "Output XML", allure.attachment_type.XML)
 
-    return context
+        return context
 
 
 @when("extract text from JSON", target_fixture="context")
 def extract_text_from_json(context, lambda_test_service):
-    actual_txt = lambda_test_service.extract_text_from_json(context["input_json"])
-    context["actual_txt"] = actual_txt
+    with allure.step("Extract Text from JSON"):
+        actual_txt = lambda_test_service.extract_text_from_json(context["input_json"])
+        context["actual_txt"] = actual_txt
+        logger.info(f"Extract Text: {context['input_json']} from JSON: {context['actual_txt']}")
+        allure.attach(context['input_json'], "Input JSON", allure.attachment_type.JSON)
+        allure.attach(context['actual_txt'], "Output TEXT", allure.attachment_type.TEXT)
 
-    return context
+        return context
 
 
 @then("comparison of expected and actual XML", target_fixture="context")
 def compare_expected_and_actual_xml(context, lambda_test_service):
-    expected_xml = context["expected_xml"]
-    actual_xml = context["actual_xml"]
-    minify_expected_xml = lambda_test_service.minify_xml(expected_xml)
-    minify_actual_xml = lambda_test_service.minify_xml(actual_xml)
+    with allure.step("Compare expected and actual XML"):
+        expected_xml = context["expected_xml"]
+        actual_xml = context["actual_xml"]
+        minify_expected_xml = lambda_test_service.minify_xml(expected_xml)
+        minify_actual_xml = lambda_test_service.minify_xml(actual_xml)
+        logger.info(f"Compare expected: {expected_xml} and actual XML: {actual_xml}")
+        logger.info(f"Compare expected: {minify_expected_xml} and actual XML: {minify_actual_xml}")
+        allure.attach(minify_expected_xml, "Minify expected XML", allure.attachment_type.XML)
+        allure.attach(minify_actual_xml, "Minify actual XML", allure.attachment_type.XML)
 
-    assert minify_expected_xml == minify_actual_xml
+        assert minify_expected_xml == minify_actual_xml
 
 
 @then("comparison of expected and actual text", target_fixture="context")
 def compare_expected_and_actual_txt(context):
-    context["actual_txt"] = context["actual_txt"].replace(" \n", "\n")
-    context["expected_txt"] = context["expected_txt"].replace(" \n", "\n")
-    assert context["actual_txt"] == context["expected_txt"]
+    with allure.step("Compare expected and actual TEXT"):
+        context["actual_txt"] = context["actual_txt"].replace(" \n", "\n")
+        context["expected_txt"] = context["expected_txt"].replace(" \n", "\n")
+        logger.info(f"Compare expected: {context['expected_txt']} and actual: {context['actual_txt']} TEXT")
+        allure.attach(context["actual_txt"], "Actual TEXT", allure.attachment_type.TEXT)
+        allure.attach(context["expected_txt"], "Expected TEXT", allure.attachment_type.TEXT)
+
+        assert context["actual_txt"] == context["expected_txt"]
 
 
 @given(parsers.parse("prepare a YAML file: {yaml_file_name}"), target_fixture="context")
 def a_yaml_file_is_prepared(yaml_file_name, context):
-    input_yaml = read_data_file(f"yaml/{yaml_file_name}")
-    context["yaml"] = input_yaml
+    with allure.step("Prepare a YAML file"):
+        context["yaml"] = read_data_file(f"yaml/{yaml_file_name}")
+        logger.info(f"Prepare a YAML file: {context['yaml']}")
+        allure.attach(yaml_file_name, "Name of YAML file name", allure.attachment_type.TEXT)
+        allure.attach(json.dumps(context["yaml"], indent=2), "Input YAML", allure.attachment_type.YAML)
 
-    return context
+        return context
 
 
 @when("validation YAML", target_fixture="context")
@@ -85,7 +120,10 @@ def validation_yaml(context, lambda_test_service):
 
 @then("comparison of expected and actual string", target_fixture="context")
 def compare_expected_and_actual_string(context):
-    assert context["response"] == "Valid YAML"
+    with allure.step("Compare expected and actual string"):
+        logger.info(f"Compare expected: {context['response']} and actual string")
+        allure.attach(context["response"], "Expected Text", allure.attachment_type.TEXT)
+        assert context["response"] == "Valid YAML"
 
 
 @when("converting XML to YAML", target_fixture="context")
@@ -106,7 +144,12 @@ def convert_json_to_yaml(context, lambda_test_service):
 
 @then("comparison of expected and actual YAML", target_fixture="context")
 def compare_expected_and_actual_yaml(context):
-    assert context["actual_yaml"] == context["yaml"]
+    with allure.step("Compare expected and actual YAML"):
+        logger.info(f"Compare expected: {context['yaml']} and actual YAML: {context['actual_yaml']}")
+        allure.attach(context["actual_yaml"], "Actual YAML", allure.attachment_type.YAML)
+        allure.attach(context["yaml"], "Expected YAML", allure.attachment_type.YAML)
+
+        assert context["actual_yaml"] == context["yaml"]
 
 
 @when("converting YAML to JSON", target_fixture="context")
@@ -119,5 +162,11 @@ def convert_yaml_to_json(context, lambda_test_service):
 
 @then("comparison of expected and actual JSON", target_fixture="context")
 def compare_expected_and_actual_json(context):
-    expected_json = json.loads(context["input_json"])
-    assert context["actual_json"] == expected_json
+    with allure.step("Compare expected and actual JSON"):
+        expected_json = json.loads(context["input_json"])
+
+        logger.info(f"Compare expected: {expected_json} and actual JSON: {context['actual_json']}")
+        allure.attach(json.dumps(expected_json, indent=2), "Expected JSON", allure.attachment_type.JSON)
+        allure.attach(json.dumps(context["actual_json"], indent=2), "Actual JSON", allure.attachment_type.JSON)
+
+        assert context["actual_json"] == expected_json
